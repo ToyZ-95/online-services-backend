bba1a1e2ba44db6237cf7f71dd06c2e8981a6eed
Asad Patel <asad.patel@thinksense.ai>
2021-05-05 17:00:13 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var multer = require('multer');
+var cors = require('cors');
+
+app.use(cors());
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' +file.originalname )
+  }
+});
+
+var upload = multer({ storage: storage }).single('file');
  
 app.get('/', function (req, res) {
-  res.send('Online Service Backend')
+  res.send('Online Service Backend');
+});
+
+app.post('/upload', function (req, res) {
+    // console.log(req.data);
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+    return res.status(200).send(req.file);
+
+ })
+
 })
+
  
-app.listen(8080)
+app.listen(8080);
