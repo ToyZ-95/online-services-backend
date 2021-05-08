a35d7a281c00155a4b5b2e2314c20039a217306d
Asad Patel <asad.patel@thinksense.ai>
2021-05-08 11:19:47 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var multer = require('multer');
 var cors = require('cors');
-
+const asyncHandler = require('express-async-handler');
 
 app.use(cors());
 
@@ -15,10 +15,10 @@ const fs = require('fs');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-    cb(null, 'public')
+    cb(null, 'public');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' +file.originalname )
+    cb(null, "input.docx" );
   }
 });
 
@@ -47,7 +47,51 @@ app.get('/download', function (req, res){
 
 }); 
 
-app.post('/upload', function (req, res) {
+app.post('/upload', asyncHandler(async(req, res) => {
+  console.log("uploading doc");
+  await uploadDoc(req,res);
+  console.log("uploaded doc");
+  console.log("converting doc");
+  await convertDoc();
+  console.log("converted doc");
+
+  const src = fs.createReadStream(__dirname + '/public/output.pdf');
+
+  src.pipe(res);
+  res.download(__dirname + '/public/output.pdf', 'output.pdf', (err) =>{
+  if(err){
+    console.log(err);
+  }
+});
+
+}));
+
+
+async function uploadDoc(req,res){
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+        return res.status(500).json(err)
+    } else if (err) {
+        return res.status(500).json(err)
+    }
+  });
+}
+
+async function convertDoc(){
+  console.log(" inside before converting doc");
+  let inputFile =  __dirname + '/public/input.docx';
+  console.log((inputFile));
+  docxConverter(inputFile,'./public/output.pdf',function(err,result){
+    if(err){
+      console.log(err);
+    }
+    console.log('result'+result);
+  });
+  console.log("inside after converting doc");
+}
+
+
+app.post('/upload123', function (req, res) {
     // console.log(req.data);
     upload(req, res, function (err) {
         if (err instanceof multer.MulterError) {
